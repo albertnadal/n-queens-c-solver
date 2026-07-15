97ac6555d99f531271d92965bc8156a247fa77a9
Albert Nadal Garriga <anadalg@gmail.com>
2026-07-15 13:03:02 +0200

    Fix super_fast_solver

--- super_fast_solver.c
+++ super_fast_solver.c
@@ -2,12 +2,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <assert.h>
 #include <omp.h>
 
-#define STORE_SOLUTIONS 1 // Set to 0 to disable storing solutions in memory
+#define STORE_SOLUTIONS 0 // Set to 0 to disable storing solutions in memory
 #define N 8 // Cannot be greater than 16 due to using uint32_t values for bitmasking
 #define MAX_SOLUTIONS 92 // Use 92 for N=8, 724 for N=10, 2680 for N=12, 14200 for N=14, 14772512 for N=16
 
@@ -55,10 +54,8 @@ void solve_n_queens(int start_x, int end_x, int y, uint32_t vertical_mask, uint3
 
   for (int x = start_x; x < end_x; x++) {
     if (is_valid_queen(x, y, vertical_mask, left_diagonal_mask, right_diagonal_mask)) {
-#if STORE_SOLUTIONS
       thread_data[thread_idx].current_solution[y * 2] = x;
       thread_data[thread_idx].current_solution[y * 2 + 1] = y;
-#endif
       solve_n_queens(0, N, y + 1, vertical_mask | (1U << x), left_diagonal_mask | (1U << (x + y)), right_diagonal_mask | (1U << (x - y + N - 1)), thread_idx, thread_data);
     }
   }
@@ -67,10 +64,12 @@ void solve_n_queens(int start_x, int end_x, int y, uint32_t vertical_mask, uint3
 int main(int argc, char *argv[]) {
   assert((N % 2) == 0 && "N value must be even.");
 
-  struct timespec start, end;
   int num_threads = (int)sysconf(_SC_NPROCESSORS_ONLN);
-  printf("Solving %d-Queen Problem with %d parallel threads.\n", N, num_threads);
+  int range_per_thread = (num_threads >= N / 2) ? 1 : (N / 2) / num_threads;
+  int max_usable_threads = (num_threads >= N / 2) ? N / 2 : num_threads;
+
   thread_data *threads_data = malloc(sizeof(thread_data) * num_threads);
+  printf("Solving %d-Queen Problem with %d parallel threads.\n", N, max_usable_threads);
 
   if (threads_data == NULL) {
     fprintf(stderr, "Error: cannot allocate memory for threads_data.\n");
@@ -81,10 +80,9 @@ int main(int argc, char *argv[]) {
     threads_data[i].solutions_count = 0;
   }
 
+  struct timespec start, end;
   clock_gettime(CLOCK_MONOTONIC, &start);
-  int range_per_thread = (num_threads >= N / 2) ? 1 : (N / 2) / num_threads;
-  int max_usable_threads = (num_threads >= N / 2) ? N / 2 : num_threads;
-#pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(dynamic)
   for (int thread_idx = 0; thread_idx < max_usable_threads; thread_idx++) {
     int start_x = thread_idx * range_per_thread;
     int end_x = (thread_idx == max_usable_threads - 1) ? N / 2 : start_x + range_per_thread;
